fa891c4592290a7874a73c66944ce2de81c5ed1b
Arunangshu Mazumdar <amazumdar41@gmail.com>
2025-06-22 22:31:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,147 @@
+const express = require('express');
+const path = require('path');
+const bcrypt = require('bcrypt');
+const userModel = require('./models/userModel');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine','ejs');
+
+app.post('/user/create',(req,res)=>{
+    let{name,email,password} = req.body;
+    bcrypt.genSalt(10,(err,salt)=>{
+    bcrypt.hash(password, salt,async function(err, hash) {
+    let user = await userModel.create({
+    name,
+    email,
+    password:hash,
+ })
+ res.json({
+    success: true,
+    message: 'User successfully created!',
+    redirectUrl: '/login'
+});
+ });
+         })
+ })
+
+app.post('/login',(req,res)=>{
+    let{email,password}= req.body;
+    let user = userModel.findOne({email:email});
+    if(user){
+        bcrypt.compare(password,user.password,(err,result)=>{
+            if(!result){
+                res.json({
+                    message:"No User Found!",
+                    redirectUrl:"/user/create"
+                })
+            }
+            else{
+                res.json({
+                    success:true,
+                    message:"Sucessfully Logged In",
+                    redirectUrl:"/profile/:user"
+                 })
+            }
+         } )}
+  })
+
+app.get('/login',(req,res)=>{
+    res.render('login');
+})
+
+app.post('/user/update',(req,res)=>{
+    let {id, name, email} = req.body;
+    userModel.findByIdAndUpdate(id, {name, email}, {new: true})
+    .then(user => {
+        if(user) {
+            res.json({
+                success: true,
+                message: 'User updated successfully!',
+                user: user
+            });
+        } else {
+            res.json({
+                success: false,
+                message: 'User not found!'
+            });
+        }
+    })
+    .catch(err => {
+        res.json({
+            success: false,
+            message: 'Error updating user!'
+        });
+    });
+})
+
+app.delete('/user/delete/:id',(req,res)=>{
+    let {id} = req.params;
+    userModel.findByIdAndDelete(id)
+    .then(user => {
+        if(user) {
+            res.json({
+                success: true,
+                message: 'User deleted successfully!'
+            });
+        } else {
+            res.json({
+                success: false,
+                message: 'User not found!'
+            });
+        }
+    })
+    .catch(err => {
+        res.json({
+            success: false,
+            message: 'Error deleting user!'
+        });
+    });
+})
+
+app.get('/user/:id',(req,res)=>{
+    let {id} = req.params;
+    userModel.findById(id)
+    .then(user => {
+        if(user) {
+            res.json({
+                success: true,
+                user: user
+            });
+        } else {
+            res.json({
+                success: false,
+                message: 'User not found!'
+            });
+        }
+    })
+    .catch(err => {
+        res.json({
+            success: false,
+            message: 'Error fetching user!'
+        });
+    });
+})
+
+app.get('/users',(req,res)=>{
+    userModel.find({})
+    .then(users => {
+        res.json({
+            success: true,
+            users: users
+        });
+    })
+    .catch(err => {
+        res.json({
+            success: false,
+            message: 'Error fetching users!'
+        });
+    });
+})
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
